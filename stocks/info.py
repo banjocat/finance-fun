3f17b861bcc0592c36fdf1a9dc99ec040c3ddb4b
banjocat <jackmuratore@gmail.com>
2017-02-11 00:46:46 -0500

    Add get stock price data

--- stocks/info.py
+++ stocks/info.py
@@ -18,6 +18,22 @@ def get_nasdaq_names():
     symbols = [ x.text for x in result ]
     return symbols
 
+def get_stock_price_data(stock):
+    '''
+    Gets stock data
+    
+    >>> stock = get_stock_price_data('GRUB')
+    >>> stock['Status']
+    u'SUCCESS'
+    '''
+    endpoint = 'http://dev.markitondemand.com/Api/v2/Quote/json'
+    url = '%s?symbol=%s' % (endpoint, stock)
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+
 
 if __name__ == '__main__':
     import doctest

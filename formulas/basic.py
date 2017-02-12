9e85d51932b02a55c3f087c35f55012b64204be8
banjocat <jackmuratore@gmail.com>
2017-02-11 20:35:16 -0500

    Cleanup

--- formulas/basic.py
+++ formulas/basic.py
@@ -2,17 +2,17 @@ import math
 
 
 def interest(rate, years):
-    '''
+    """
     Interest formula with N = 1
 
     >>> val = interest(.01, 1)
     >>> round(val, 2)
     1.01
-    '''
+    """
     return math.pow(1 + rate, years)
 
 def compound(rate, compound_amount, years):
-    '''
+    """
     compound interest with N = 1
 
     >>> val = compound(.08, 4, 5)
@@ -24,13 +24,13 @@ def compound(rate, compound_amount, years):
     >>> val = 100 * compound(.05, 1, 2)
     >>> round(val, 2)
     110.25
-    '''
+    """
     rate = float(rate) / compound_amount
     result = math.pow(1 + rate, years * compound_amount)
     return result
 
 def continous_compound(rate, years):
-    '''
+    """
     theortical continous compound rate
 
     >>> val = continous_compound(.05, 1)
@@ -42,20 +42,9 @@ def continous_compound(rate, years):
     >>> val = continous_compound(.08, 5)
     >>> round(val, 4)
     1.4918
-    '''
+    """
     rT = rate * years
     return math.pow(math.e, rT)
-
-def zcb(rate, current_years, mature_years):
-    '''
-    zero coupond bond rate
-
-    >>> val = zcb(.05, 2, 1)
-    >>> round(val, 2)
-    1.1
-    '''
-    rTt = -1 * rate * (mature_years - current_years)
-    return math.pow(math.e, rTt)
 
 
 if __name__ == '__main__':
--- stocks/info.py
+++ stocks/info.py
@@ -1,16 +1,15 @@
 import requests
-from lxml import html
 from bs4 import BeautifulSoup
 
 
 def get_nasdaq_names():
-    '''
+    """
     Gets an array of all nasdaq stocks
 
     >>> symbols = get_nasdaq_names()
     >>> len(symbols)
     105
-    '''
+    """
     url = 'http://www.cnbc.com/nasdaq-100'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -19,13 +18,13 @@ def get_nasdaq_names():
     return symbols
 
 def get_sp500_names():
-    '''
+    """
     Gets an array of all sp500 stocks
 
     >>> symbols = get_sp500_names()
     >>> len(symbols)
     505
-    '''
+    """
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -34,13 +33,13 @@ def get_sp500_names():
     return symbols
 
 def get_stock_price_data(stock):
-    '''
+    """
     Gets stock data
     
     >>> stock = get_stock_price_data('GRUB')
     >>> stock['Status']
     u'SUCCESS'
-    '''
+    """
     endpoint = 'http://dev.markitondemand.com/Api/v2/Quote/json'
     url = '%s?symbol=%s' % (endpoint, stock)
     response = requests.get(url)
